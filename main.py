4be81f25a9572842425f72dab0c26963abea7acb
eyyupguner <48321198+eyyupguner@users.noreply.github.com>
2023-10-27 12:06:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 
-url = 'https://docs.google.com/spreadsheets/d/1Tq5KEhwBO9FSlcftABHNeFKmSBtVP2cMznapsgqrYzI/gviz/tq?tqx=out:csv&sheet=RH_C'
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzpMf_ifwbgNjR0O4IaSnqz1_fd9TzKezLFehi9SJLpa-hs38qjh0Qr3wvXjcBez_25wnE4qufDtHr/pub?gid=0&single=true&output=csv'
 df = pd.read_csv(url)
 
 app = FastAPI()
