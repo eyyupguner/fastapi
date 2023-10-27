35e2490915889260f23c0ba36f59b60c03585e64
eyyupguner <48321198+eyyupguner@users.noreply.github.com>
2023-10-27 14:30:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzpMf_ifwbgNjR0O4IaSnqz1_fd9TzKezLFehi9SJLpa-hs38qjh0Qr3wvXjcBez_25wnE4qufDtHr/pub?gid=0&single=true&output=csv'
@@ -6,6 +7,15 @@ df = pd.read_csv(url)
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
